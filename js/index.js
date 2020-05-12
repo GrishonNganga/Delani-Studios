9819ae3c2bb5630687c00485f63f8e8751f549b0
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-05-12 17:52:01 +0300

    Add necessary form validations and mailchimp implementation.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
-$('document').ready(function(){
+function checkNotNull(value){
+    if(value == ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function danger(user, email, message){
+    if(checkNotNull(user)){
+        $('#username').addClass('danger');
+    }else{
+        $('#username').removeClass('danger');
+    }
+    if(checkNotNull(email)){
+        $('#email').addClass('danger');
+    }else{
+        $('#email').removeClass('danger');
+    }
+    if(checkNotNull(message)){
+        $('#message').addClass('danger');
+    }else{
+        $('#message').removeClass('danger');
+    }
+}
+
+function removeDanger(){
+    $('#username').removeClass('danger');
+    $('#email').removeClass('danger');
+    $('#message').removeClass('danger');
+}
+
+$(document).ready(function(){
     
     $('#design').click(function(){
         $('#design-img').toggle();
@@ -22,4 +54,16 @@ $('document').ready(function(){
         });
     });
 
+    $('form.validate').submit(function(event){
+        var user = document.getElementById('username').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('message').value;
+
+        if(checkNotNull(user) || checkNotNull(email) || checkNotNull(message)){
+            danger(user, email, message);
+        }else{
+            removeDanger();
+            alert('Thank you '+user+' for reaching out. We will get back to you.');
+        }
+    });
 });
